70c28e9f4818d57f0ca7453cbeacadfb2c6a50cd
Gaurav Chauhan <107812523+grvchnx@users.noreply.github.com>
2026-07-29 06:51:38 +0530

    fix: unintended log cleanup on build failure

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
+PDF_NAME=gaurav_chauhan     # set pdf name without spaces
 
-PDF_NAME=gaurav_chauhan     # your name without spaces
-
-mkdir -p build
-pdflatex -synctex=1 -interaction=nonstopmode -output-directory=build main.tex
-cp build/main.pdf "${PDF_NAME}.pdf"
+mkdir -p build && \
+pdflatex -synctex=1 -interaction=nonstopmode -output-directory=build main.tex && \
+cp build/main.pdf "${PDF_NAME}.pdf" && \
 rm -rf build
